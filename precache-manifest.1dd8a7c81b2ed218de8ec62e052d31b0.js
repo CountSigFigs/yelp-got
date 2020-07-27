90485e303f7a218ffe2aeddd73abefd87cce88c0
Brian Holt <bharrisonholt@gmail.com>
2020-07-27 16:37:56 -0700

    Updates

---
+++ precache-manifest.1dd8a7c81b2ed218de8ec62e052d31b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7455800f6d5020fa2bedf9d7b0312f4",
+    "url": "/CountSigFigs/yelp-got/index.html"
+  },
+  {
+    "revision": "89c0fd4314017f7cc868",
+    "url": "/CountSigFigs/yelp-got/static/css/main.e8538eb4.chunk.css"
+  },
+  {
+    "revision": "c0df735aed10bb993f3a",
+    "url": "/CountSigFigs/yelp-got/static/js/2.8ab5959e.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/CountSigFigs/yelp-got/static/js/2.8ab5959e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c0fd4314017f7cc868",
+    "url": "/CountSigFigs/yelp-got/static/js/main.1ce3db1b.chunk.js"
+  },
+  {
+    "revision": "b95c0de51b70277c7849",
+    "url": "/CountSigFigs/yelp-got/static/js/runtime-main.3be426cf.js"
+  },
+  {
+    "revision": "a196bcb68e5229ee8bc2889c551cb0c2",
+    "url": "/CountSigFigs/yelp-got/static/media/tyrion.a196bcb6.png"
+  }
+]);
